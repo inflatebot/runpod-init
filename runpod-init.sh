149e7c13f027bd92e80bbb9aed89cd35e44af54d
inflatebot <alfanumeric@protonmail.com>
2024-10-28 01:50:53 -0400

    yolo

--- runpod-init.sh
+++ runpod-init.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # basic utils
 #https://raw.githubusercontent.com/inflatebot/runpod-init/refs/heads/main/runpod-init.sh
-export HF_HOME=/workspace
-yes | unminimize 
-apt install -y nano tmux htop nvtop whiptail
 
-# setup HF
-pip install huggingface_hub[cli]
-git config --global credential.helper store
-yes | huggingface-cli login
+basic() {
+	export HF_HOME=/workspace
+	yes | unminimize 
+	apt install -y nano tmux htop nvtop whiptail
+	pip install huggingface_hub[cli]
+	git config --global credential.helper store
+}
+
+hf-login() {
+	yes | huggingface-cli login
+}
 
 install-axolotl() {
-	cd /workspace
+	cd /workspace || exit
 	# burg time
 	git clone https://github.com/axolotl-ai-cloud/axolotl
-	cd axolotl
+	cd axolotl || exit
 	# checkout current working commit
 	git checkout 6d9a3c4d817cd57e702b270c04d2b2d2400c3ad4
 	python3 -m venv venv; source venv/bin/activate
@@ -23,18 +27,18 @@ install-axolotl() {
 }
 
 install-llamafactory() {
-	cd /workspace
+	cd /workspace || exit
 	git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
-	cd LLaMA-Factory
+	cd LLaMA-Factory || exit
 	tput bel
 	echo -e "---\nSelect which extra dependencies, as a comma separated list with no spaces, to pass to pip from these available ones (torch and metrics are required):\ntorch,metrics,torch-npu,deepspeed,liger-kernel,bitsandbytes,hqq,eetq,gptq,awq,aqlm,vllm,galore,badam,adam-mini,qwen,modelscope,openmind,quality\n---\n"
 	read -e -i "torch,metrics,adam-mini,deepspeed,liger-kernel,bitsandbytes,hqq" -p "$PS2 " deps
 }
 
 install-llamacpp() {
-	cd /workspace
+	cd /workspace || exit
 	git clone https://github.com/ggerganov/llama.cpp.git
-	cd llama.cpp
+	cd llama.cpp || exit
 	export LLAMA_CPP_DIR=/workspace/llama.cpp
 	make GGML_CUDA=1 -j 8 
 	# set up venv for lcpp
@@ -43,21 +47,21 @@ install-llamacpp() {
 }
 
 install-vllm() {
-	cd /workspace
+	cd /workspace || exit
 	mkdir vllm
-	cd vllm
+	cd vllm || exit
 	python3 -m venv venv; source venv/bin/activate
 	pip install vllm
 }
 
 install-aphrodite() {
-	cd /workspace
+	cd /workspace || exit
 	mkdir aphrodite
-	cd aphrodite
+	cd aphrodite || exit
 	python3 -m venv venv; source venv/bin/activate
 	pip install aphrodite-engine
 }
-echo "hello"
+
 basic
 
 # set up ggify
