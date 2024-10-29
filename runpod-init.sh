945707a2417bb8fe24bc2d154c503bb7341934f1
inflatebot <alfanumeric@protonmail.com>
2024-10-28 22:39:13 -0400

    llamafactory function wasn't making a venv

--- runpod-init.sh
+++ runpod-init.sh
@@ -26,6 +26,7 @@ install-llamafactory() {
 	\"torch,metrics,torch-npu,deepspeed,liger-kernel,bitsandbytes,hqq,eetq,gptq,\nawq,aqlm,vllm,galore,badam,adam-mini,qwen,modelscope,openmind,quality\"" 16 80 \
 	"torch,metrics,adam-mini,deepspeed,liger-kernel,bitsandbytes,hqq" \
  	3>&1 1>&2 2>&3) && echo "$deps"
+	python3 -m venv venv; source venv/bin/activate
 	python3 -m pip install -e ".[$deps]"
 	#echo -e "---\nSelect which extra dependencies, as a comma separated list with no spaces, to pass to pip from these available ones (torch and metrics are required):\ntorch,metrics,torch-npu,deepspeed,liger-kernel,bitsandbytes,hqq,eetq,gptq,awq,aqlm,vllm,galore,badam,adam-mini,qwen,modelscope,openmind,quality\n---\n"
 	#read -e -i "torch,metrics,adam-mini,deepspeed,liger-kernel,bitsandbytes,hqq" -p "$PS2 " deps
